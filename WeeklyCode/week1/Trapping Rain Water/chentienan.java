96b073b8a86f81adf5ca51b6c3eb526482ec1343
coderfengyun <chentienan@yeah.net>
2014-04-20 23:09:46 +0800

    refactor
    refactor

--- WeeklyCode/week1/Trapping Rain Water/chentienan.java
+++ WeeklyCode/week1/Trapping Rain Water/chentienan.java
@@ -1,49 +1,44 @@
 public class Solution {
     public int trap(int[] A) {
         if(A.length < 3) return 0;
-        int firstTurningPoint = -1, secondTurnningPoint = -1, result = 0;
-        for(int i = 1; i < A.length - 1 ; i++)
+        return trap(A, 0 , A.length - 1);
+    }
+    
+    private int trap(int[] A, int begin, int end)
+    {
+        if(end - begin < 2) return 0;
+        int maxValueIndex = A[begin] > A[begin + 1] ? begin : begin + 1, secondMaxValueIndex = A[begin] > A[begin + 1] ? begin + 1 : begin;
+        for(int i = begin + 2; i <= end; i++)
         {
-            if(firstTurningPoint != -1 && secondTurnningPoint != -1)
+            if(A[i] > A[maxValueIndex]) 
             {
-                result += calculateTheTrapBetweenTwoTurnningPoint(firstTurningPoint, secondTurnningPoint);
-                firstTurningPoint = -1;
-                secondTurnningPoint = -1;
+                secondMaxValueIndex = maxValueIndex;
+                maxValueIndex = i;
             }
-            else 
+            else if(A[i] > A[secondMaxValueIndex])
             {
-                if(isTurnningPoint(i, A))
-                {
-                    if(firstTurningPoint == -1) firstTurningPoint = i;
-                    else   //The secondTurnningPoint is -1
-                    {
-                        
-                        secondTurnningPoint = i;
-                    }
-                }
+                secondMaxValueIndex = i;
             }
+        }
+        int minorOne = maxValueIndex > secondMaxValueIndex ? secondMaxValueIndex : maxValueIndex;
+        int largerOne = maxValueIndex > secondMaxValueIndex ? maxValueIndex : secondMaxValueIndex;
+        return calculateTheTrapBetweenTwoTurnningPoint(minorOne, largerOne, A) + trap(A, begin, minorOne) + trap(A, largerOne, end);
+    }
+    
+    private int calculateTheTrapBetweenTwoTurnningPoint(int firstTurnningPoint, int secondTurnningPoint, int[] A)
+    {
+        if(secondTurnningPoint - firstTurnningPoint < 2) return 0;
+        int min = getMin(A[firstTurnningPoint], A[secondTurnningPoint]);
+        int result = (secondTurnningPoint - firstTurnningPoint -1) * min;
+        for(int i = firstTurnningPoint + 1; i < secondTurnningPoint; i++)
+        {
+            result -= A[i];
         }
         return result;
     }
     
-    private int calculateTheTrapBetweenTwoTurnningPoint(int firstTurningPoint, int secondTurnningPoint, int[] A)
+    private int getMin(int first, int second)
     {
-        //TODO: fulfill this function
-        return -1;
-    }
-    
-    private boolean isTurnningPoint(int index, int[] A)
-    {
-        if(index == 0)
-        {
-            return A[0] > A[1];
-        }
-        else if(index == A.length -1)
-        {
-            return A[A.length -1] > A[A.length -2]
-        }
-        else{
-            return A[i] >= A[i - 1] && A[i] >= A[i + 1];
-        }
+        return first >= second ? second : first;
     }
 }
