cc4711e350ab7502e0ba678cc27dc21740fc3a01
wupeachfan <136901774@qq.com>
2014-04-20 21:21:42 +0800

    Create wufan.cpp

---
+++ WeeklyCode/week1/Remove Duplicates from Sorted Array II/wufan.cpp
@@ -0,0 +1,21 @@
+class WeekOne{
+public:
+  int removeDuplicates(int A[], int n) {
+		if (n < 2) return n;
+		int pre = A[0];
+		int cnt = 1;
+		int index = 0;
+		for (int i = 1; i<n; i++){
+			if(A[i] != pre){
+				pre = A[i];
+				cnt = 1;
+				A[++index] = A[i];
+			}else if(cnt < 2){
+				cnt ++;
+				A[++index] = A[i];
+			}
+		}
+		return index+1;
+}
+
+};
