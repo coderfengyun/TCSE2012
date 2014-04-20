621bb2d82628fab63beaca6b975fd7432cb25c35
coderfengyun <chentienan@yeah.net>
2014-04-20 23:48:35 +0800

    refactor
    refactor

--- WeeklyCode/week1/Valid Sudoku/chentienan.java
+++ WeeklyCode/week1/Valid Sudoku/chentienan.java
@@ -5,7 +5,7 @@ public class Solution {
             for(int column = 0; column < 9; column++)
             {
                 if(board[row][column] == '.') continue;
-                if(checkFor(row, column, board)) return false;                
+                if(!checkFor(row, column, board)) return false;                
             }
         }
         return true;
@@ -13,28 +13,25 @@ public class Solution {
     
     private boolean checkFor(int row, int column, char[][] board)
     {
-        if(column < 9)
+        //check for row where the element stays
+        for(int i = column + 1; i < 9; i++)
         {
-            //check for row where the element stays
-            for(int i = column + 1; i < 9; i++)
+            if(board[row][i] == board[row][column]) return false;
+        }
+        
+        //check for column where the element stays
+        for(int i = row + 1; i < 9; i++)
+        {
+            if(board[i][column] == board[row][column]) return false;
+        }
+        
+        //check for 9 size cell where the element stays
+        int cellRow = row / 3, cellColumn = column / 3;
+        for(int i = row + 1; i <= 2 + 3 * cellRow; i++)
+        {
+            for(int j = column + 1; j <= 2 + 3 * cellColumn; j++)
             {
-                if(board[row][i] == board[row][column]) return false;
-            }
-            
-            //check for column where the element stays
-            for(int i = row + 1; i < 9; i++)
-            {
-                if(board[i][column] == board[row][column]) return false;
-            }
-            
-            //check for 9 size cell where the element stays
-            int cellRow = row / 3, cellColumn = column / 3;
-            for(int i = row + 1; i < 2 + 3 * cellRow; i++)
-            {
-                for(int j = column + 1; j < 2 + 3 * cellColumn; j++)
-                {
-                    if(board[i][j] == board[row][column]) return false;
-                }
+                if(board[i][j] == board[row][column]) return false;
             }
         }
         return true;
