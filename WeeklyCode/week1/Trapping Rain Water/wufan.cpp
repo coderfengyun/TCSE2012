ad61a9de3dd8c4ef676f136e6b33b321cb55802f
wupeachfan <136901774@qq.com>
2014-04-20 21:38:27 +0800

    Update wufan.cpp

--- WeeklyCode/week1/Trapping Rain Water/wufan.cpp
+++ WeeklyCode/week1/Trapping Rain Water/wufan.cpp
@@ -1,29 +1,27 @@
 class WeekOne{
 public:
-  int trap(int A[], int n){
-		 int i = 0;
-		 int j = n-1;
-		 int water = 0;
-		 int max_left = A[i];
-		 int max_right = A[j];
-		 while (i < j){
-			 if (A[i] < A[j]){
-				 i++;
-				 if (A[i] < max_left)
-					 water += max_left - A[i];
-				 else
-					 max_left = A[i];
-			 }
-			 else{
-				 j--;
-				 if (A[j] < max_right)
-					 water += max_right - A[j];
-				 else
-					 max_right = A[j];
-			 }
-		 }
-		 return water;
-  }
-
-
+   int trap(int A[], int n){
+        int i = 0;
+        int j = n-1;
+        int water = 0;
+        int max_left = A[i];
+        int max_right = A[j];
+        while (i < j){
+            if (A[i] < A[j]){
+                i++;
+                if (A[i] < max_left)
+                    water += max_left - A[i];
+                else
+                    max_left = A[i];
+            }
+            else{
+                j--;
+                if (A[j] < max_right)
+                    water += max_right - A[j];
+                else
+                    max_right = A[j];
+            }
+        }
+        return water;
+    }
 };
