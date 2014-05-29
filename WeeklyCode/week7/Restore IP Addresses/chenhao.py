35d14ae6c74c172c93a0e608e96a62dcc6f61ec6
chenhao <chenh1024@gmail.com>
2014-05-30 00:43:12 +0800

    update Restore IP Addresses

--- WeeklyCode/week7/Restore IP Addresses/chenhao.py
+++ WeeklyCode/week7/Restore IP Addresses/chenhao.py
@@ -1,20 +1,15 @@
 class Solution:
     
     def dfs(self,s,cur,arr,res):
-        if cur>=len(s):
-            if len(arr)==4:
-                res.append('.'.join(arr))
+        if len(arr)==4 and cur>=len(s):
+            res.append('.'.join(arr))
             return
-        if len(arr)==4:
-            return
-        if cur>=len(s):
+        if len(arr)==4 or cur>=len(s):
             return
         for length in range(1,min(3,len(s)-cur)+1):
             sub=s[cur:cur+length]
             val=int(sub)
-            if val<0 or val>255:
-                continue
-            if len(str(val))!=length:
+            if val<0 or val>255 or len(str(val))!=length:
                 break;
             arr.append(sub)
             self.dfs(s,cur+length,arr,res)
@@ -26,4 +21,3 @@ class Solution:
         res=[]
         self.dfs(s,0,[],res)
         return res
-        
