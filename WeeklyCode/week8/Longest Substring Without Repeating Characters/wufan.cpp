df825a8252c209da86392fc315a45b4efdd76e80
wupeachfan <136901774@qq.com>
2014-06-06 21:46:35 +0800

    Create wufan.cpp

---
+++ WeeklyCode/week8/Longest Substring Without Repeating Characters/wufan.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+       if (s.length() == 0) return 0;
+       map<char, int> exsited;
+       int cur_len = 0, max_len = 0;
+       int start = 0;
+       for (int i = 0; i < s.length(); i++){
+           char c = s[i];
+           if (exsited.find(c) == exsited.end()){
+                cur_len++;
+           }else{
+               if (exsited[c] < start){
+                   cur_len++;
+               }else{
+                   cur_len = i - exsited[c];
+                   start = exsited[c]+1;
+               }
+           }
+           exsited[c] = i;
+           if (cur_len > max_len) max_len = cur_len;
+       }
+       return max_len;
+    }
+
+};
