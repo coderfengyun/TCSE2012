0169a760b3d20312a4f72a750da04d7dd671b3bf
Shellon <lxl.shellon@gmail.com>
2014-06-16 11:30:28 +0800

    fix word break II

--- WeeklyCode/week9/Word Break II/liuxianglong.java
+++ WeeklyCode/week9/Word Break II/liuxianglong.java
@@ -1,43 +1,30 @@
 class liuxianglong {
-	private void dfsWordBreak(String s, boolean seg[][], int start,
-			ArrayList<String> ret, StringBuilder sb, Set<String> dict) {
-		if (s.equals("")) {
-			ret.add(sb.substring(0, sb.length() - 1));
-		}
-		for (int len = 1; len <= s.length(); len++) {
-			if (seg[start][len]) {
-				String str = s.substring(0, len);
-				if (dict.contains(str)) {
-					sb.append(str).append(" ");
-					dfsWordBreak(s.substring(len), seg, start + len, ret, sb, dict);
-					sb.delete(sb.length() - str.length() - 1, sb.length());
-				}
-			}
-		}
-	}
-	public ArrayList<String> wordBreak(String s, Set<String> dict) {
-		ArrayList<String> result= new ArrayList<String>();
-		if (s.equals("")) return result;
-		boolean seg[][] = new boolean[s.length()][s.length() + 1];
-		for (int len = 1; len <= s.length(); len++) {
-			for (int i = 0; i < s.length(); i++) {
-				if (i + len <= s.length()) {
-					if (dict.contains(s.substring(i, i + len))) {
-						seg[i][len] = true;
-						continue;
-					}
-					for (int k = 1; k < len; k++) {
-						if (seg[i][k] && seg[i + k][len - k]) {
-							seg[i][len] = true;
-							break;
-						}
-					}
-				}
-			}
-		}
-		if (!seg[0][s.length()])
-			return result;
-		dfsWordBreak(s, seg, 0, result, new StringBuilder(), dict);
-		return result;
-	}
+	private void dfs(ArrayList<String> res, String s,boolean[][] record, int cur,String raw){
+        if(cur==0){
+            res.add(s.substring(0,s.length()-1));
+        }else{
+            for(int i=0;i<cur;i++){
+                if(record[cur][i]){
+                    String next = raw.substring(i,cur)+" "+s;
+                    dfs(res,next,record,i,raw);
+                }
+            }
+        }
+    }
+    public ArrayList<String> wordBreak(String s, Set<String> dict) {
+        ArrayList<String> res = new ArrayList<>();
+        if(s.length()==0) return res;
+        boolean[] dp = new boolean[s.length()+1];dp[0]=true;
+        boolean[][] record = new boolean[s.length()+1][s.length()+1];
+        for(int i =0;i<s.length();i++){
+            for(int j=0;j<=i;j++){
+                boolean tmp =dp[j]&&dict.contains(s.substring(j,i+1));
+                if(!tmp) continue;
+                dp[i+1]=true;
+                record[i+1][j]=true;
+            }
+        }
+        dfs(res,"",record,s.length(),s);
+        return res;
+    }
 }
